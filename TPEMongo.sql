56114d0cad177a8db61d54c4999ad561030aadf2
ElPela0 <agrillo@alumnos.exa.unicen.edu.ar>
2024-10-23 21:48:28 -0300

    pedilo

--- TPEMongo.sql
+++ TPEMongo.sql
@@ -1,12 +1,33 @@
 db.servicio.aggregate([
 {
     $group: {
-        _id: "$tipoIntervalo",
-        cantidad: {$sum: 1}
-    }},{
-    $project: {
-        _id:1,
-        cantidad:1
+      _id: "$tipoIntervalo",
+      cantidad: { $sum: 1 }
     }
+},
+{
+    $sort: {
+      cantidad: 1
+    }
+}
+]);
 
-}]);
+db.comprobante.aggregate([
+{
+    $group:{
+        _id: "$cliente.idCliente",
+        cant: {$sum:1},
+        totalFacturacion:{$sum:"$importe"}
+    }
+},
+{
+    $match:{
+        totalFacturacion:{$gt:250}
+    }
+},
+{
+    $sort:{
+        totalFacturacion:-1
+    }
+}
+])
